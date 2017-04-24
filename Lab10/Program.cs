0aa1a5202659c75b44477285297c5fab6f25bb0f
alsoraimi <alsoraimi46@gmail.com>
2017-04-24 14:11:57 -0400

    add foreach loop to determine which category and prints out movies

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -15,19 +15,28 @@ namespace Lab10
             Console.WriteLine("There are 10 Movies in this list");
 
 
-            Movies[] MovieList = new Movies[10];
+            Movies[] MovieList = new Movies[10];      // made array for List of movies
             MovieList[0]= new Movies ("Star Wars" ,"SciFi" );
             MovieList[1]= new Movies ("2001: A Space Odyssey", "SciFi");
             MovieList[2]= new Movies ("Saw", "Horror" );
             MovieList[3]= new Movies ("Scream", "Horror");
-            MovieList[4]= new Movies ("Saving Private Ryan", "Drama");
+            MovieList[4]= new Movies ("Saving Private Ryan", "Drama");  //same format of property
             MovieList[5]= new Movies ("The GodFather", "Drama");
             MovieList[6]= new Movies ("Up", "Animated");
             MovieList[7]= new Movies ("Finding Nemo", "Animated");
             MovieList[8]= new Movies ("Happy Gilmore", "Comedy");
             MovieList[9]= new Movies ("Super Troopers", "Comedy");
 
-            Console.WriteLine(MovieList);
+            Console.WriteLine("Please enter a Category from this list: Scifi, Horror, Drama, Animated, Comedy");
+            string input = Console.ReadLine().ToUpper();
+            foreach (Movies item in MovieList)   // foreach loop to go through each movie in movie list
+            {
+                if (input == item.GCategory)    //if statement for which category they picked
+                {
+                    Console.WriteLine($"{item.GTitle}");  // prints out the movies of certain category
+                }
+            }
+ 
 
                  
 
