850ba900d5a10405f2ac0d5e078263cb0794a58a
alsoraimi <alsoraimi46@gmail.com>
2017-04-24 11:44:39 -0400

    initial commit, added class for movie with properties

---
+++ Lab10/Movies.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Movies
+    {
+        private string Title;
+        private string Category;
+
+        public string GTitle
+        {
+            set
+            {
+                Title = value;
+            }
+            get
+            {
+                return Title;
+            }
+
+        }
+
+        public string GCategory
+        {
+            set
+            {
+                Category = value;
+            }
+            get
+            {
+                return Category;
+            }
+        }
+
+    }
+}
